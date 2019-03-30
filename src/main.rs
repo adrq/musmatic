ef7c3eebbd1c00c21cf895393ac95deda2d9c887
adrq <git@adrq.net>
2019-03-30 14:03:50 -0400

    add license header

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
  * 
- * Copyright (c) 2019
- * Authors: Adrian Quiroga
+ * Copyright (c) 2019 - Musmatic authors
  */
 use std::ffi::CString;
 use std::ffi::CStr;
