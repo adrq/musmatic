348ae7d986a4e250e5dffdcd51ca6ed46f1578f5
adrq <git@adrq.net>
2020-11-11 17:32:16 -0500

    fix test for toast

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2019-2020 - Musmatic authors
+ */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders containerToast', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const containerToast = screen.getByText(/App container/i);
+  expect(containerToast).toBeInTheDocument();
 });
