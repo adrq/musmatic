ceed5b222e9848daaaa6afaf46830eb6d8417842
adrq <git@adrq.net>
2020-11-11 09:37:30 -0500

    install react w/ typescript

---
+++ app/src/App.tsx
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ *
+ * Copyright (c) 2019-2020 - Musmatic authors
+ */
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default App;
