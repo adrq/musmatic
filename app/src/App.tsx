3e379f9745a0979c1c6c4cdbc86f58c4386476a7
adrq <git@adrq.net>
2020-12-30 11:20:33 -0500

    added import to App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,13 +5,24 @@
  * Copyright (c) 2019-2020 - Musmatic authors
  */
 
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Flex } from '@chakra-ui/core';
 import './App.scss';
 import ScoreViewer from './components/ScoreViewer/ScoreViewer';
 
+declare const window: any;
+
 function App() {
   const parentEl = useRef(null);
+
+  // HACK - wait for wasm to finish compiling/loading
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('Initializing verovio toolkit');
+      const vrvToolkit = new window.verovio.toolkit();
+      console.log(vrvToolkit);
+    }, 2000);
+  });
   return (
     <Flex bg="darkgray" direction="column" color="white" ref={parentEl}>
       App container
