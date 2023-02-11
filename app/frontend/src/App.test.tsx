4d140ffc6cb56b859133a3770f2538389aa2e62b
adrq <git@adrq.net>
2023-02-11 14:36:57 -0500

    update copyright year

--- app/frontend/src/App.test.tsx
+++ app/frontend/src/App.test.tsx
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2019-2022 - Musmatic authors
+ * Copyright (c) 2019-2023 - Musmatic authors
  */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
--- app/frontend/src/utils.js
+++ app/frontend/src/utils.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2019-2022 - Musmatic authors
+ * Copyright (c) 2019-2023 - Musmatic authors
  */
 
 export function getElementWidth(el) {
