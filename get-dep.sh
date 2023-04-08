2838aeab20addd8b0e79b097dab242d54ae08d79
adrq <git@adrq.net>
2023-04-07 22:48:54 -0400

    update dep fetch script

--- get-dep.sh
+++ get-dep.sh
@@ -2,6 +2,8 @@
 
 DIRECTORY="vendor/verovio"
 
+COMMIT="d58523025b6b6b72eaf68beb050d47f4c775d7f8"
+
 #check if verovio already cloned
 if [ -d "$DIRECTORY" ]; then
   echo "Already cloned. Updating."
@@ -9,21 +11,19 @@ if [ -d "$DIRECTORY" ]; then
   git checkout develop
   git reset --hard HEAD
   git pull
-  cd tools
-  ./get_git_commit.sh
-
-  #fix CMake recipe to make a static library instead of shared
-  sed -i -e 's/SHARED/STATIC/g' CMakeLists.txt
-  exit
+  git checkout $COMMIT
+else
+  echo "Cloning git repo"
+  #if not then we clone repo
+  git clone --depth 1 https://github.com/adrq/verovio.git $DIRECTORY
+  cd $DIRECTORY
+  git checkout $COMMIT
 fi
 
-echo "Cloning git repo"
-#if not then we clone repo
-git clone --depth 1 https://github.com/rism-ch/verovio.git $DIRECTORY
-cd $DIRECTORY
-git checkout develop
 cd tools
 ./get_git_commit.sh
 
 #fix CMake recipe to make a static library instead of shared
+cd ../cmake
 sed -i -e 's/SHARED/STATIC/g' CMakeLists.txt
+
